0d1a86ed6080dfd1ab0d17c2f6f1b47570b69657
conmurao263 <conmurao263@gmail.com>
2025-03-11 10:43:08 +0000

    Add 6.php

---
+++ src/6.php
@@ -0,0 +1,28 @@
+<?php
+
+// Function to generate a random number between 1 and 10
+function getRandomNumber() {
+    return rand(1, 10);
+}
+
+// Function to calculate the sum of two numbers
+function addNumbers($num1, $num2) {
+    return $num1 + $num2;
+}
+
+// Function to calculate the difference of two numbers
+function subtractNumbers($num1, $num2) {
+    return $num1 - $num2;
+}
+
+// Function to calculate the product of two numbers
+function multiplyNumbers($num1, $num2) {
+    return $num1 * $num2;
+}
+
+// Function to calculate the quotient of two numbers
+function divideNumbers($num1, $num2) {
+    return $num1 / $num2;
+}
+
+?>
