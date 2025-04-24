4a0d5260e95c60c6d71303952a99a30ceb9049c4
conmurao263 <conmurao263@gmail.com>
2025-04-24 02:57:51 +0000

    Add 28.php

---
+++ src/28.php
@@ -0,0 +1,10 @@
+<?php
+// Example PHP code to solve a simple math problem
+$problem = "What is $a + $b?"; // Input from user
+$a = 3; // Input value of variable 'a'
+$b = 5; // Input value of variable 'b'
+
+$result = $a + $b;
+
+echo $result;
+?>
