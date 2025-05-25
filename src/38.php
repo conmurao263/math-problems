83504fcc20037fc936eb59303b4606fc8d200ea6
conmurao263 <conmurao263@gmail.com>
2025-05-25 06:33:23 +0000

    Add 38.php

---
+++ src/38.php
@@ -0,0 +1,14 @@
+<?php
+// This is an example PHP script.
+// It will not compile and run as-is. 
+
+function sumOfNumbers($a, $b) {
+    return $a + $b;
+}
+
+// Test function
+echo "The sum of 5 and 3 is: " . sumOfNumbers(5, 3); // Output: The sum of 5 and 3 is: 8
+
+$studentName = "John Doe";
+echo "Hello, $studentName!";
+?>
